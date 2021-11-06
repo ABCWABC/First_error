4e85ae4466eb7a4e5e7e1810514871a016135000
l123 <ymlee092345@gmail.com>
2021-11-06 19:22:50 +0900

    baek_개_정답 (\와 " 는 앞에 \를 각각 전부 붙여야함)

---
+++ src/A002/개$baek.java
@@ -0,0 +1,14 @@
+package A002;
+
+public class 개$baek {
+
+	public static void main(String[] args) {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
