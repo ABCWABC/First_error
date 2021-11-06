794a6512aae8b2cd413ca0db90240d24d71df324
l123 <ymlee092345@gmail.com>
2021-11-06 19:33:11 +0900

    baek_A+B_정답 (콘솔에서 값을 입력받을때 Scanner(System.in)과 nextInt() 사용)

---
+++ src/A002/AplusB.java
@@ -0,0 +1,23 @@
+package A002;
+
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+import java.util.Scanner;
+
+public class AplusB {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		if(a>0 && a<10) {
+			if(b>0 && b<10) {
+				System.out.println(a+b);
+			}
+		}
+	}
+
+}
