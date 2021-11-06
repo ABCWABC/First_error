3feb1495399064730b20092550959c610c65a39b
l123 <ymlee092345@gmail.com>
2021-11-06 19:15:07 +0900

    baek_고양이_정답 (\를 쓰려면 \\처럼 앞에 \를 붙이는게 포인트)

---
+++ src/A002/고양이$baek.java
@@ -0,0 +1,13 @@
+package A002;
+
+public class 고양이$baek {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
